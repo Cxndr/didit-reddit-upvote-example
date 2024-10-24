358339273e8f706ebf6360c81e4a5110b2492996
Cxndr <livewellandmakethings@gmail.com>
2024-10-24 03:03:47 +0100

    added floating menu to tiptap

--- src/components/TipTapEditor.jsx
+++ src/components/TipTapEditor.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { BubbleMenu, useEditor, EditorContent } from '@tiptap/react'
+import { FloatingMenu, BubbleMenu, useEditor, EditorContent } from '@tiptap/react'
 import Document from '@tiptap/extension-document'
 import Paragraph from '@tiptap/extension-paragraph'
 import Text from '@tiptap/extension-text'
@@ -11,7 +11,10 @@ import Strike from '@tiptap/extension-strike'
 import BlockQuote from '@tiptap/extension-blockquote'
 import CharacterCount from '@tiptap/extension-character-count'
 import Placeholder from '@tiptap/extension-placeholder'
-import { useEffect, useState } from 'react'
+import BulletList from '@tiptap/extension-bullet-list'
+import ListItem from '@tiptap/extension-list-item'
+import Code from '@tiptap/extension-code'
+import { useEffect } from 'react'
 
 
 export default function TipTapEditor({content="", placeholder="", onContentChange}) {
@@ -31,12 +34,14 @@ export default function TipTapEditor({content="", placeholder="", onContentChang
       Italic,
       BlockQuote,
       Strike,
+      BulletList,
+      ListItem,
+      Code,
       CharacterCount.configure({ limit: 1000 }),
       BubbleMenu,
       Placeholder.configure({
         placeholder: `${placeholder}`,
       }),
-      
     ],
     editorProps: {
       attributes: {
@@ -56,22 +61,52 @@ export default function TipTapEditor({content="", placeholder="", onContentChang
       <div className="control-group">
       </div>
 
+      {editor && <FloatingMenu editor={editor} tippyOptions={{ duration: 100, offset:[0,200] }}>
+        <div className="floating-menu">
+          <button
+            type="button"
+            onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+            className={editor.isActive('heading', { level: 1 }) ? 'is-active' : ''}
+          >
+            H1
+          </button>
+          <button
+            type="button"
+            onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+            className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}
+          >
+            H2
+          </button>
+          <button
+            type="button"
+            onClick={() => editor.chain().focus().toggleBulletList().run()}
+            className={editor.isActive('bulletList') ? 'is-active' : ''}
+          >
+            Bullet list
+          </button>
+        </div>
+        </FloatingMenu>
+      }
+
       {editor && 
         <BubbleMenu editor={editor} tippyOptions={{ duration: 100 }}>
           <div className="bubble-menu">
             <button
+              type="button"
               onClick={() => editor.chain().focus().toggleBold().run()}
               className={editor.isActive('bold') ? 'is-active' : ''}
             >
               Bold
             </button>
             <button
+              type="button"
               onClick={() => editor.chain().focus().toggleItalic().run()}
               className={editor.isActive('italic') ? 'is-active' : ''}
             >
               Italic
             </button>
             <button
+              type="button"
               onClick={() => editor.chain().focus().toggleStrike().run()}
               className={editor.isActive('strike') ? 'is-active' : ''}
             >
